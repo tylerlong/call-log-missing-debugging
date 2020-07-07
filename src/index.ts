867d9cde4fa9e2666f0075e1015a1a46f1b40f1a
Tyler Long <tyler4long@gmail.com>
2020-07-07 15:14:50 -0700

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world');
+import RingCentral from 'ringcentral-extensible';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL!,
+  token: {access_token: process.env.RINGCENTRAL_ACCESS_TOKEN!},
+});
+
+(async () => {
+  const extInfo = await rc.restapi().account().extension().get();
+  console.log(extInfo);
+})();
